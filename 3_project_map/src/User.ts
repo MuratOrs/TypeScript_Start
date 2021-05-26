e1e0c44f9985973911618178b94bc2fc01c4711a
Murat Ors <webschool2015@gmail.com>
2021-05-26 13:47:02 +0200

    export generated data

--- 3_project_map/src/User.ts
+++ 3_project_map/src/User.ts
@@ -1,13 +1,22 @@
 import faker from 'faker'
 
-class User {
+//export default 'DefExpo' - you can export everything by default, 
+//but this practice is not encouraged in TS
+
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
   constructor() {
-    
+    this.name = faker.name.findName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
   }
 }
+
+
 
--- 3_project_map/src/index.ts
+++ 3_project_map/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hallo');
+import { User } from './User'
+
+const user = new User();
+// generating data of random people
+console.log(user);
+
+
+
+
+
+
+  
