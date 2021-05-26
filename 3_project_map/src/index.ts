6912610fb5dbd2ff787c0ee9802f453e66f214d8
Murat Ors <webschool2015@gmail.com>
2021-05-26 15:36:26 +0200

    added class Company

--- 3_project_map/src/index.ts
+++ 3_project_map/src/index.ts
@@ -1,8 +1,13 @@
+import { Company } from './Company';
 import { User } from './User'
 
 const user = new User();
 // generating data of random people
 console.log(user);
+
+
+const company = new Company();
+console.log(company);
 
 
 
